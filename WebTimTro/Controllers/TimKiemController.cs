5d3c6f5b81947cf37946fd5754ca35e45d2ff38f
jndoser <hoanglongdev533@gmail.com>
2022-05-20 05:31:32 +0700

    Thêm tính năng tìm kiếm phòng trọ lân cận - hiển thị các phòng trọ trong phạm vi 1km xung quanh vị trí mà người dùng nhập vào, hiển thị popup khi click vào các marker của phòng trọ lân cận, và cho phép xem chi tiết phòng trọ đó thông qua đường link trong popup

--- WebTimTro/Controllers/TimKiemController.cs
+++ WebTimTro/Controllers/TimKiemController.cs
@@ -1,12 +1,77 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebTimTro.Interfaces;
+using WebTimTro.Models;
 
 namespace WebTimTro.Controllers
 {
     public class TimKiemController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public TimKiemController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetPhongTroLanCan()
+        {
+            List<string> coords = new List<string>();
+            var phongTros = _unitOfWork.PhongTro.GetAll();
+
+            foreach(var phongTro in phongTros)
+            {
+                string coord = phongTro.Latitude + "," + phongTro.Longitude;
+                coords.Add(coord);
+            }
+
+            return Json(new { status = "ok", data = coords });
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetPhongTroNearby(decimal lat, decimal lng)
+        {
+            DistanceResponseModel distances = new DistanceResponseModel();
+            var phongTros = _unitOfWork.PhongTro.GetAll();
+            string apiKey = "E5ycgkTiRNvLJ7BqyM2H4kC0LJWGqbJKK2eVWUQH";
+            string destinationUrlPart = "";
+            foreach(var phongTro in phongTros)
+            {
+                destinationUrlPart += phongTro.Latitude + "," + phongTro.Longitude + "%7C";
+            }
+            destinationUrlPart = destinationUrlPart.Substring(0, destinationUrlPart.Length - 4);
+            string restAPIUrl = $"https://rsapi.goong.io/DistanceMatrix?origins={lat},{lng}&destinations={destinationUrlPart}&vehicle=car&api_key={apiKey}";
+            //string apiResponse = "";
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(restAPIUrl))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    distances = JsonConvert.DeserializeObject<DistanceResponseModel>(apiResponse);
+                }
+            }
+
+            // Lấy ra khoảng cách đến các phòng trọ khác
+            List<int> khoangCachs = new List<int>();
+            foreach(var item in distances.rows[0].elements)
+            {
+                khoangCachs.Add(item.distance.value);
+            }
+
+
+            return Json(new { status = "ok", phongTros = phongTros, khoangCachs = khoangCachs });
         }
     }
 }
---
+++ WebTimTro/Models/DistanceResponseModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace WebTimTro.Models
+{
+    public class DistanceResponseModel
+    {
+        public Row[] rows { get; set; }
+    }
+
+    public class Row
+    {
+        public Element[] elements { get; set; }
+    }
+
+    public class Element
+    {
+        public string status { get; set; }
+        public Duration duration { get; set; }
+        public Distance distance { get; set; }
+    }
+
+    public class Duration
+    {
+        public string text { get; set; }
+        public int value { get; set; }
+    }
+
+    public class Distance
+    {
+        public string text { get; set; }
+        public int value { get; set; }
+    }
+}
